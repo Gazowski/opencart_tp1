d8faae0430181ab5eef3fa7cd2ce47f955c8dfa4
Gazowski <gael_comeau@hotmail.com>
2019-11-06 08:12:21 -0500

    ajout de .gitignore

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://e1895410.webdev.cmaisonneuve.qc.ca/session_4/solution_web/tp1_gestion_des_nouvelles/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://e1895410.webdev.cmaisonneuve.qc.ca/session_4/solution_web/tp1_gestion_des_nouvelles/');
+
+// DIR
+define('DIR_APPLICATION', '/home/e1895410/www/session_4/solution_web/tp1_gestion_des_nouvelles/catalog/');
+define('DIR_SYSTEM', '/home/e1895410/www/session_4/solution_web/tp1_gestion_des_nouvelles/system/');
+define('DIR_IMAGE', '/home/e1895410/www/session_4/solution_web/tp1_gestion_des_nouvelles/image/');
+define('DIR_STORAGE', '/home/e1895410/www/session_4/solution_web/tp1_gestion_des_nouvelles/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'e1895410');
+define('DB_PASSWORD', 'zq5ddLLVAw8qCjcUqJBN');
+define('DB_DATABASE', 'e1895410');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'tp1_');
